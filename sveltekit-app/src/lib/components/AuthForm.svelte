2a364655ebbe1bd12ad53ce0a5155043d19e8905
Alexander Mortasen <axmortasen@gmail.com>
2024-03-13 14:09:51 +0200

    Create test auth page

---
+++ sveltekit-app/src/lib/components/AuthForm.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let title = 'Sign in to platform';
+	export let rememberMe = false;
+	export let lostPassword = false;
+	export let alternative = false;
+	export let alternativeText = '';
+	export let alternativeLink = '';
+	export let alternativeLinkText = '';
+	export let lostPasswordLink = '';
+	export let submitButtonLabel = 'Login to your account';
+
+	export let mainClass = 'bg-gray-50 dark:bg-gray-900 w-full';
+	export let mainDivClass =
+		'flex flex-col items-center justify-center p-24 mx-auto pt:mt-0 dark:bg-gray-900';
+	export let cardH1Class = 'text-2xl font-bold text-gray-900 dark:text-white text-center';
+</script>
+
+<div class={mainClass}>
+	<div class={mainDivClass}>
+		<!-- Card -->
+		<div class="w-full !p-10 max-w-md">
+			<h1 class={cardH1Class}>
+				{title}
+			</h1>
+			<form class="mt-8 space-y-6" on:submit|preventDefault>
+				<slot />
+				{#if rememberMe || lostPassword}
+					<div class="flex items-start">
+						{#if rememberMe}
+							<input type="checkbox" name="remember">Remember me</input>
+						{/if}
+						{#if lostPassword}
+							<a href={lostPasswordLink} class="ml-auto text-sm hover:no-underline hover:text-white transition">Lost Password?</a>
+						{/if}
+					</div>
+				{/if}
+				<div class="flex justify-center">
+					<button type="submit">{submitButtonLabel}</button>
+				</div>
+				{#if alternative}
+					<div class="text-md text-center text-gray-500 dark:text-gray-400">
+						{alternativeText} <a href={alternativeLink} class="hover:no-underline hover:text-white transition">{alternativeLinkText}</a>
+					</div>
+				{/if}
+			</form>
+		</div>
+	</div>
+</div>
---
+++ sveltekit-app/src/routes/auth/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { goto } from '$app/navigation'
+
+	import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+	const auth = getAuth()
+
+	import AuthForm from "$lib/components/AuthForm.svelte";
+
+	let email = '';
+	let password = '';
+	function handleSignIn () {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				console.log('Signed in', user)
+				goto('/dashboard')
+			})
+			.catch((error) => {
+				console.log('ERROR', error.code, error.message)
+			})
+	}
+</script>
+
+<AuthForm
+	title="Sign in to platform"
+	rememberMe="true"
+	lostPassword="true"
+	lostPasswordLink="restore"
+	submitButtonLabel="Login to your account"
+	alternative="true"
+	alternativeText="Not registered?"
+	alternativeLink="sign-up"
+	alternativeLinkText="Create account"
+	on:submit={handleSignIn}
+>
+	<div>
+		<label for="email" class="mb-2">Your email</label>
+		<input bind:value={email} type="email" name="email" id="email" placeholder="name@company.com" required class="transition duration-300 ease-in-out" />
+	</div>
+	<div>
+		<label for="password" class="mb-2">Your password</label>
+		<input bind:value={password} type="password" name="password" id="password" placeholder="••••••••" required class="transition duration-300 ease-in-out" />
+	</div>
+</AuthForm>
