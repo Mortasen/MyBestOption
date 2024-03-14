46c3cea4c5de2b603d1142fb3105fa83644d822d
Alexander Mortasen <axmortasen@gmail.com>
2024-03-14 11:24:08 +0200

    Add back button to layout

--- sveltekit-app/src/routes/+layout.svelte
+++ sveltekit-app/src/routes/+layout.svelte
@@ -6,10 +6,16 @@
 	onMount(() => (ready = true));
 </script>
 
-<div class="dragbar"></div>
 <div class="w-screen h-screen overflow-hidden flex flex-col bg-gray-50">
-	<div class="p-4 text-center font-semibold bg-teal-400 shadow-xl">
-		<h1 class="text-xl">BestChoice</h1>
+	<div class="min-h-16 p-4 text-center bg-teal-400 shadow-xl flex relative">
+		<div class="w-full text-xl text-center font-semibold">
+			<h1>BestChoice</h1>
+		</div>
+		<div class="absolute">
+			<a href="/" class="px-2 py-1 bg-teal-600 rounded-lg block hover:bg-teal-700 text-white transition">
+				Back
+			</a>
+		</div>
 	</div>
 	{#if ready}
 		<div class="flex-grow min-h-0 p-4">
