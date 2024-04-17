ce1eb90ced6916bc6bdb0e7fcb491093a3b99651
Alexander Mortasen <axmortasen@gmail.com>
2024-04-17 19:20:53 +0300

    Improve AuthForm, remove extra code

--- sveltekit-app/src/lib/components/AuthForm.svelte
+++ sveltekit-app/src/lib/components/AuthForm.svelte
@@ -1,17 +1,28 @@
 <script>
+	// Label displayed on top
 	export let title = 'Sign in to platform';
+	// Controls whether to show remmeber me checkbox
 	export let rememberMe = false;
+	// Controls whether to show lost password link
 	export let lostPassword = false;
+	// Controls whether to show alternative text and link
 	export let alternative = false;
+	// Text to display for alternative link
 	export let alternativeText = '';
+	// Link to the alternative
 	export let alternativeLink = '';
+	// Text to display for the alternative link
 	export let alternativeLinkText = '';
+	// Link to the lost password page
 	export let lostPasswordLink = '';
+	// Label for the submit button
 	export let submitButtonLabel = 'Login to your account';
-
+	// Classes for the form
 	export let mainClass = 'bg-gray-50 w-full';
+	// Classes for the main div
 	export let mainDivClass =
 		'flex flex-col items-center justify-center mx-auto pt:mt-0';
+	// Classes for the card h1
 	export let cardH1Class = 'text-2xl font-bold text-center';
 </script>
 
@@ -24,18 +35,18 @@
 			</h1>
 			<form class="mt-8 flex flex-col gap-6" on:submit|preventDefault>
 				<slot />
-				<!--{#if rememberMe || lostPassword}-->
-				<!--	<div class="flex items-start">-->
-				<!--		{#if rememberMe}-->
-				<!--			<label>-->
-				<!--				<input type="checkbox" name="remember" class="text-primary-500 mr-2 size-4">Remember me-->
-				<!--			</label>-->
-				<!--		{/if}-->
-				<!--		{#if lostPassword}-->
-				<!--			<a href={lostPasswordLink} class="ml-auto hover:no-underline transition">Lost Password?</a>-->
-				<!--		{/if}-->
-				<!--	</div>-->
-				<!--{/if}-->
+				{#if rememberMe || lostPassword}
+					<div class="flex items-start">
+						{#if rememberMe}
+							<label>
+								<input type="checkbox" name="remember" class="text-primary-500 mr-2 size-4">Remember me
+							</label>
+						{/if}
+						{#if lostPassword}
+							<a href={lostPasswordLink} class="ml-auto hover:no-underline transition">Lost Password?</a>
+						{/if}
+					</div>
+				{/if}
 				<button type="submit" class="px-4 py-3 rounded-lg font-medium">
 					{submitButtonLabel}
 				</button>
